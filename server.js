ad35d99f44dae08126199573689ebcde869f9512
autoserviciovictor <autoserviciovictor.nqn@gmail.com>
2026-06-10 10:30:51 -0300

    numero server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ Mensaje del cliente: ${text}
       {
         messaging_product: "whatsapp",
         recipient_type: "individual",
-        to: from,
+        to: "542994654375",
         type: "text",
         text: {
           preview_url: false,
